b05dee6f8f33b88e158ecb9620f8eebbe167b619
AlaeddineJendoubi <36207352+AlaeddineJendoubi@users.noreply.github.com>
2021-09-20 01:17:54 +0100

    update associate player to club

--- src/utils/get-players-data/index.ts
+++ src/utils/get-players-data/index.ts
@@ -1,11 +1,11 @@
 import { map, compact, isNil, filter } from "lodash";
 import { updatePlayerAction } from "../../actions/players-action";
-import { Players, Player, Club } from "../../types";
+import { Players, Player } from "../../types";
 
 /**
- * Transforms a club object to item object
- * @param  club club object
- * @returns A transformed club object
+ * Transforms a player object to item object
+ * @param  player player object
+ * @returns A transformed player object
  */
 export const transformPlayerData = (player: Player) => {
   return {
@@ -14,10 +14,11 @@ export const transformPlayerData = (player: Player) => {
     clubs: player?.clubs,
   };
 };
+
 /**
- * Gets clubs data  depending on selected season
- * @param  clubs clubs array
- * @returns an array of transformed clubs data
+ * Gets players data  depending on selected season
+ * @param  players players array
+ * @returns an array of transformed players data
  */
 export const playersData = (players: Players) => {
   return compact(
@@ -26,6 +27,13 @@ export const playersData = (players: Players) => {
     })
   );
 };
+
+/**
+ * Retruns selected player data
+ * @param  selectedIndex selected player index
+ * @param  players players array
+ * @returns an array of transformed players data
+ */
 export const selectedPlayerData = (selectedIndex, players) => {
   try {
     return isNil(players[selectedIndex]) ? null : players[selectedIndex];
@@ -34,24 +42,30 @@ export const selectedPlayerData = (selectedIndex, players) => {
   }
 };
 
-export const updatePlayerData = (dispatch, players, selectedPlayer, clubID) => {
-  const newAsoociatedClub = [{ id: clubID }];
+/**
+ * Retruns selected player data
+ * @param  dispatch redux dispatch hook
+ * @param  players players array
+ * @param  selectedPlayer selected player data
+ * @param  players players array
+ * @param  season selected season object
+ * @returns updates selected club state
+ */
+export const updatePlayerData = (
+  dispatch,
+  players,
+  selectedPlayer,
+  clubID,
+  season
+) => {
+  const newAsoociatedClub = [{ season: season?.id, club: clubID }];
   const updatedPlayerData = {
     ...selectedPlayer,
     clubs: [...selectedPlayer?.clubs, ...newAsoociatedClub],
   };
-
   const playersWithoutUpdatedPlayer = filter(players, (player) => {
     return player?.id !== selectedPlayer?.id;
   });
-
   const updatedPlayers = [...playersWithoutUpdatedPlayer, updatedPlayerData];
-
   dispatch(updatePlayerAction(updatedPlayers));
 };
-
-// export const updatePlayersState = (players,updatedPlayer)=>{
-//   map(players,player =>{
-//     if(player)
-//   })
-// }
