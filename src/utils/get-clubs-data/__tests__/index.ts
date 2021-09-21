154ec82be902bef306ec53568c7a0043a46b5f2f
AlaeddineJendoubi <36207352+AlaeddineJendoubi@users.noreply.github.com>
2021-09-21 07:54:15 +0100

    update get clubs data tests with new mock

--- src/utils/get-clubs-data/__tests__/index.ts
+++ src/utils/get-clubs-data/__tests__/index.ts
@@ -7,17 +7,11 @@ import {
 } from "../index";
 import { state } from "../../__mocks__/";
 const stateMock = state;
-const clubMock = {
-  id: "adade",
-  name: "EST",
-  logo: 1,
-  country: "Tunisia",
-  seasons: [{ id: 1 }, { id: 2 }],
-};
+const clubMock = state?.clubs[0];
 
 test("transform club data to item format", () => {
   expect(transformClubData(clubMock)).toEqual({
-    id: "adade",
+    id: "ClubID1",
     title: "EST",
     logo: 1,
     country: "Tunisia",
@@ -26,22 +20,22 @@ test("transform club data to item format", () => {
 
 test("Transform all clubs data to item format", () => {
   expect(clubsData(stateMock?.clubs)).toEqual([
-    { country: "Tunisia", id: "adade", logo: 1, title: "EST" },
-    { country: "Tunisia", id: "adadqwde", logo: 2, title: "CA" },
-    { country: "Tunisia", id: "adasdaade", logo: 3, title: "ESS" },
+    { country: "Tunisia", id: "ClubID1", logo: 1, title: "EST" },
+    { country: "Tunisia", id: "ClubID2", logo: 2, title: "CA" },
+    { country: "Tunisia", id: "ClubID3", logo: 3, title: "ESS" },
   ]);
 });
 
 test("Filter clubs data depending on selected season", () => {
   expect(filteredClubsData(stateMock)).toEqual([
-    { country: "Tunisia", id: "adade", logo: 1, title: "EST" },
-    { country: "Tunisia", id: "adadqwde", logo: 2, title: "CA" },
+    { country: "Tunisia", id: "ClubID1", logo: 1, title: "EST" },
+    { country: "Tunisia", id: "ClubID2", logo: 2, title: "CA" },
   ]);
 });
 
 test("Gets selected club detail by ID", () => {
-  expect(getClubDetailsByID("adade", state?.clubs)).toEqual({
-    id: "adade",
+  expect(getClubDetailsByID("ClubID1", state?.clubs)).toEqual({
+    id: "ClubID1",
     name: "EST",
     logo: 1,
     country: "Tunisia",
@@ -59,72 +53,31 @@ test("Filters players per selected club", () => {
   ).toEqual([
     {
       clubs: [
-        {
-          club: "adade",
-          season: 1,
-        },
-        {
-          club: "adadqwde",
-          season: 2,
-        },
+        { club: "ClubID1", season: 1 },
+        { club: "ClubID2", season: 2 },
       ],
-      id: "234321ac",
+      id: "PlayerID1",
       lastName: "Holmes",
       name: "Blake",
       nbrGoals: [
-        {
-          club: "adade",
-          nbr: 50,
-          season: 1,
-        },
-        {
-          club: "adadqwde",
-          nbr: 3,
-          season: 2,
-        },
+        { club: "ClubID1", nbr: 5, season: 1 },
+        { club: "ClubID2", nbr: 3, season: 1 },
+        { club: "ClubID2", nbr: 3, season: 2 },
       ],
       nbrMatch: [
-        {
-          club: "adade",
-          nbr: 3,
-          season: 1,
-        },
-        {
-          club: "adadqwde",
-          nbr: 7,
-          season: 1,
-        },
-        {
-          club: "adasdaade",
-          nbr: 23,
-          season: 3,
-        },
+        { club: "ClubID1", nbr: 1, season: 1 },
+        { club: "ClubID1", nbr: 7, season: 2 },
+        { club: "ClubID2", nbr: 4, season: 3 },
+        { club: "ClubID2", nbr: 2, season: 1 },
       ],
     },
     {
-      clubs: [
-        {
-          club: "adade",
-          season: 1,
-        },
-      ],
-      id: "3sad1",
+      clubs: [{ club: "ClubID1", season: 1 }],
+      id: "PlayerID3",
       lastName: "Reed",
       name: "Alina",
-      nbrGoals: [
-        {
-          club: "adade",
-          nbr: 5,
-          season: 1,
-        },
-      ],
-      nbrMatch: [
-        {
-          club: "adade",
-          nbr: 0,
-          season: 1,
-        },
-      ],
+      nbrGoals: [{ club: "ClubID3", nbr: 5, season: 1 }],
+      nbrMatch: [{ club: "ClubID2", nbr: 0, season: 1 }],
     },
   ]);
 });
